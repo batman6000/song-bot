d4d296bae8973361db451743fa1ff1f5b66d0ac7
batman6000 <85685179+batman6000@users.noreply.github.com>
2022-01-27 09:06:01 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -8,7 +8,13 @@ import os
 import time
 from config import Config
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+ABS="Developer"
+APPER="MirrorToIndex"
 OWNER="Owner"
+GITCLONE="https://t.me/MTSLUpdates"
+B2="telegram.dog/MirrorToIndex"
+BUTTON1="📜 BOT UPDATES 📜"
 
 def time_to_seconds(time):
     stringt = str(time)
@@ -20,7 +26,10 @@ async def start(client, message):
          reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(OWNER, url=f"https://https://t.me/{Config.OWNER}"),
+                    InlineKeyboardButton(BUTTON1, url=GITCLONE)
+                 ],[
+                    InlineKeyboardButton(OWNER, url=f"https://telegram.dog/{Config.OWNER}"),
+                    InlineKeyboardButton(ABS, url=B2)
             ]
           ]
         ),
@@ -58,7 +67,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[@haveuskslm]" 
+            performer = f"[@MirrorToIndex]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
@@ -73,13 +82,13 @@ def a(client, message):
         )
         print(str(e))
         return
-    m.edit("`Uploading... Please Wait 1 Min...`")
+    m.edit("` Uploading... Please Wait Min.....`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 <b>Title:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n📻 <b>Uploaded By:</b> <a href="https://t.me/@aveuskslm">KAVI</a>'
+        rep = f'🎶 <b>Title:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n📻 <b>Uploaded By:</b> <a href="https://t.me/songsdownloader2022bot">KAVI</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
